4633c0f4470bc2d59e47893401cabe30a7f1a494
Adedapo.Adeseye <dapsymigo001@yahoo.com>
2021-11-23 07:38:06 +0000

    production branch commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let myFunction = () => {
+    document.getElementById('change').innerHTML = 'Hello world';
+}
+let myFunction1 = () => {
+    document.getElementById("heading").style.color = 'green'
+};
+
+
+let myFunction2 = () => {
+    document.getElementsByTagName('li')[2].style.color = "blue"
+};
+let myFunction3 = () => {
+document.getElementById("List").style.color = "red"};
+
+let myFunction4 = () => {
+    document.getElementById("currencylist").style.color = "green"};
